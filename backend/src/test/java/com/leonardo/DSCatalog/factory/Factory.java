801829a9128e1ea8919856a4beaa7c4c9589adbe
Leonardo <leomeireles66@gmail.com>
2025-02-14 06:58:54 -0300

    Test updateShouldReturnProductDTOWhenIdExists passed

--- backend/src/test/java/com/leonardo/DSCatalog/factory/Factory.java
+++ backend/src/test/java/com/leonardo/DSCatalog/factory/Factory.java
@@ -11,7 +11,7 @@ public class Factory {
 
     public static Product createProduct(){
         Product product = new Product(1L, "Phone", "Good Phone", 500.00, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
-        product.getCategories().add(new Category(2L, "Eletronics"));
+        product.getCategories().add(new Category(1L, "Eletronics"));
         return product;
     }
 
